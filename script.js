25c87ff887b90d5326b205df050c34d7819c1881
Syed Izzat Ullah <53023816+SyedIzzatUllah@users.noreply.github.com>
2022-04-25 22:43:27 +0500

    Add files via upload
    js

---
+++ script.js
@@ -0,0 +1,7 @@
+let result = document.querySelector("h1");
+let btn = document.querySelector("button");
+
+
+btn.addEventListener("click", () => {
+          result.innerText = Math.trunc(Math.random() * 101);
+});
